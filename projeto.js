e2c33afe906956a7f6320d11dd484595c7f7474f
kauaneiras <kauante@hotmail.com>
2022-06-30 15:44:02 -0300

    Alteração JS: Comparar se os valores recebidos na função goToPage2Quizz() são validos

--- projeto.js
+++ projeto.js
@@ -33,5 +33,40 @@ function createQuizz() {
     document.querySelector(".seusquizzes").style.display = "none";
     document.querySelector(".quizzes").style.display = "none";
     document.querySelector(".create-quizz-page1").classList.remove("escondido");
-    
 }
+
+//TESTAR SE FOI RECEBIDO UM LINK NA IMAGEM
+function checkUrl(str){
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);  
+}
+//CHECAR SE OS DADOS RECEBIDOS ESTÃO COM VALORES CERTOS, SALVAR E DIRECIONAR PARA A PRÓXIMA MÁGINA
+function goToPage2Quizz() {
+    quizzObject.title = document.querySelector(".quizz-data .quizz-title").value;
+    quizzObject.image = document.querySelector(".quizz-data .url-quizz)").value;
+    quantity.questions = document.querySelector(".quizz-data .questions-quantity").value;
+    quantity.levels = document.querySelector(".quizz-data .level-quantity").value;
+
+    if ((quizzObject.title.length > 19 && quizzObject.title.length < 66) && 
+        (checkUrl(quizzObject.image)) && (quantity.questions > 2) && (quantity.levels > 1)) {
+        createQuestions();}
+
+    if (!(quizzObject.title.length >= 20 && form.title.length <= 65)) {
+        alert("Titudo do Quizz deve ter entre 20 e 65 caracteres");}
+
+    else if (!(checkUrl(quizzObject.image))) {
+        alert("Isso não é um link de imagem");}
+
+    else if (quantity.questions < 3) {
+        alert("O Quizz deve ter no mínimo 3 perguntas");}
+
+    else if (quantity.levels < 2) {
+        alert("O Quizz deve ter no mínimo 2 níveis");}  
+}
+
+function createQuestions(){}
